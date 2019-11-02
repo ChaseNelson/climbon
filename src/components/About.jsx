f86dafa808514c08bec7456e454550c11c5b6739
Krista Strzeszkowski <krista.cathleen.722@gmail.com>
2019-11-01 23:43:14 -0500

    created about page containing test button

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class About extends Component {
+  render() {
+    return <Button> click here</Button>;
+  }
+}
+
+export default About;
