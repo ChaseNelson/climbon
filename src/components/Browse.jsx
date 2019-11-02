f9dadde151a2959d0e9577b92e3c14f81aa973b2
Krista Strzeszkowski <krista.cathleen.722@gmail.com>
2019-11-01 23:44:00 -0500

    created all pages and test items inside

---
+++ src/components/Browse.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class Browse extends Component {
+  render() {
+    return <Button> click here</Button>;
+  }
+}
+
+export default Browse;
